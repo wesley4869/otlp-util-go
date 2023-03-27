a5adab791fb42dfb023db579181dd3af7d2e4d63
Dennis Wang <dennis.wang@bluextrade.com>
2023-03-27 19:21:16 +0800

    [27520] Expose ForceFlush method for TracerProvider to immediately export all spans

--- init.go
+++ init.go
@@ -2,10 +2,12 @@ package otlp_util
 
 import (
 	"go.opentelemetry.io/otel"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 )
 
 var global_tracer trace.Tracer
+var global_tracer_provider *sdktrace.TracerProvider
 
 // init() initialize global_tracer to a noop tracer.
 func init() {
